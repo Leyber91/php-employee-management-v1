4ebd8b98f2c1cecdd4728f6c308a8f37d05d7b96
Pablo Caravaca <pablocaravacacalvo@gmail.com>
2021-02-11 23:57:52 +0100

    remove comments

--- assets/js/modules/service/employee-service.js
+++ assets/js/modules/service/employee-service.js
@@ -3,7 +3,6 @@ import { update } from "../components/table.js";
 export const EMPLOYEE_URL = 'http://localhost/php-employee-management-v1/src/library/employeeController.php';
 
 export function deleteEmployee(id) {
-    console.log(`Deleting ${id}`);
     axios.delete(EMPLOYEE_URL, {
         params: {
             id: id
