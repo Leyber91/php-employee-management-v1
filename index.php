044621e6154ae64330f57799c77807686b4fbbdb
Jan Puig <janpuigcapella@gmail.com>
2021-02-15 12:40:53 +0100

    login dessign

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
 <!-- TODO Application entry point. Login view -->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/css/login.css">
+    <title>Employee Management</title>
+</head>
+
+<body>
+    <section class="login">
+        <img src='https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light&width=300&height=300' />
+        <form action="./src/library/loginController.php" method="post">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email">
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password">
+            <input type="submit" value="Submit">
+        </form>
+    </section>
+</body>
+
+</html>
