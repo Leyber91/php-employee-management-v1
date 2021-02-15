37e4bb1fae2124a1df623d2cf99668363270b107
Jan Puig <janpuigcapella@gmail.com>
2021-02-15 13:19:48 +0100

    added avatar

--- index.php
+++ index.php
@@ -7,12 +7,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./assets/css/login.css">
+    <script src="./assets/js/index.js" type="module"></script>
     <title>Employee Management</title>
 </head>
 
 <body>
     <section class="login">
-        <img src='https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light&width=300&height=300' />
+        <div id='image'></div>
         <form action="./src/library/loginController.php" method="post">
             <label for="email">Email:</label>
             <input type="email" id="email" name="email">
@@ -22,5 +23,4 @@
         </form>
     </section>
 </body>
-
 </html>
